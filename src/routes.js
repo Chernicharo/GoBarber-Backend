70b5ab8196c779af16fc9a93c231a9dfaece3a77
Roberto Chernicharo <foxtamega@gmail.com>
2019-06-29 00:14:13 -0300

    user model added and tested

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'test' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'roberto',
+    email: 'email@mail.com',
+    password_hash: '22222222',
+  });
+  return res.json(user);
 });
 
 export default routes;
