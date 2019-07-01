91d7a8a9b9894313eaba339ef8334aa6f4683818
Roberto Chernicharo <foxtamega@gmail.com>
2019-07-01 19:41:30 -0300

    user update created

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -25,7 +25,33 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'user already exist' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Old password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
